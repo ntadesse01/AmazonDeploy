c0a8cc8167c1519dbef3827fee5b0c2aa56c6a06
ntadesse01 <hasetmamo01@gmail.com>
2024-07-19 13:25:31 -0700

    frontend

--- amazevaclone_2024/src/Api/axios.js
+++ amazevaclone_2024/src/Api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: `http://127.0.0.1:5001/amaevapro-fc2fd/us-central1/api`,
-    baseURL: `http://localhost:5000`,
+    // baseURL: `http://localhost:5000`,
+    baseURL: `https://amazondeploy-nov.onrender.com`
 });
 
 export { axiosInstance };
